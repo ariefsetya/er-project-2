d351f194aca890ceef956f42fbd8f523fdc8a768
Arief Setya <ariefsetya@live.com>
2019-11-18 13:13:11 +0700

    assets

--- public/resources/views/layouts/guest.blade.php
+++ public/resources/views/layouts/guest.blade.php
@@ -24,11 +24,6 @@
         </div>
 </body>
 
-<script type="text/javascript">
-document.addEventListener("DOMContentLoaded", function(event) {
-  console.log("DOM fully loaded and parsed");
-});
-</script>
 <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 @yield('footer')
