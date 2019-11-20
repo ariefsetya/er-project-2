617af4e7d28e6e6e5631c3f538e4045a6357ce19
Arief Setya <ariefsetya@live.com>
2019-11-20 16:09:48 +0800

    update

--- public/resources/views/quiz_response/result.blade.php
+++ public/resources/views/quiz_response/result.blade.php
@@ -19,7 +19,7 @@
     <div id="winner_box_{{$i}}" class="text-center col-md-6" style="display: none;">
       <h2 style="font-size:80px;text-transform: uppercase;font-weight: bold;">{!! ordinal($polling->max_winner>0?($i+1):0) !!} Winner</h2>
       <h3 id="name_{{$i}}" style="font-size:100px;text-transform: uppercase;"></h3>
-      <h3 id="company_{{$i}}" style="font-size:120px;text-transform: uppercase;"></h3>
+      <h3 id="company_{{$i}}" style="font-size:90px;text-transform: uppercase;"></h3>
       <h5 id="created_at_{{$i}}" style="font-size:60px;text-transform: uppercase;"></h5>
     </div>
    @endfor
