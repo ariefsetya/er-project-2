2457c59f4a121a88d26f860ef52e58222db0f8da
Arief Setya <ariefsetya@live.com>
2019-11-25 11:26:53 +0700

    update website title

--- public/resources/views/layouts/guest.blade.php
+++ public/resources/views/layouts/guest.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>AQUA JAPAN | 2019 ANNUAL DEALERS GATHERING</title>
+    <title>{{\App\EventDetail::where('name','website_title')->first()->content}}</title>
 
     <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.min.css') }}">
