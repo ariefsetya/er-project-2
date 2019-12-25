2666614b600b47ef07601ef5dc470ec623aaf476
Arief Setya <ariefsetya@live.com>
2019-12-25 07:58:08 +0700

    update admin

--- public/resources/views/admin.blade.php
+++ public/resources/views/admin.blade.php
@@ -7,7 +7,7 @@
 	<div class="col-md-6">
 		<div class="card mb-3">
 		  <div class="card-header text-white bg-primary text-center">
-		  	<h1>{{number_format((sizeof(\App\Presence::where('invitation_id','>',0)->where('event_id',Session::get('event_id'))->groupBy('invitation_id'))/(\App\Invitation::where('event_id',Session::get('event_id'))->where('user_type_id',1)->count()))*100,2)}}%</h1>
+		  	<h1>{{number_format((sizeof(\App\Presence::where('invitation_id','>',0)->where('event_id',Session::get('event_id'))->groupBy('invitation_id')->toArray())/(\App\Invitation::where('event_id',Session::get('event_id'))->where('user_type_id',1)->count()))*100,2)}}%</h1>
 		  </div>
 		  <div class="card-body">
 			  <div class="row">
@@ -17,7 +17,7 @@
 			  	</div>
 			  	<div class="col-md-6 text-center">
 			  		<div class="col-md-12">CHECK IN</div>
-			  		<div class="col-md-12"><h2><strong>{{sizeof(\App\Presence::where('invitation_id','>',0)->where('event_id',Session::get('event_id'))->groupBy('invitation_id')->count())}}</strong></h2></div>
+			  		<div class="col-md-12"><h2><strong>{{sizeof(\App\Presence::where('invitation_id','>',0)->where('event_id',Session::get('event_id'))->groupBy('invitation_id')->toArray())}}</strong></h2></div>
 			  	</div>
 			  </div>
 		  </div>
@@ -26,7 +26,7 @@
 	<div class="col-md-6">
 		<div class="card mb-3">
 		  <div class="card-header text-white bg-primary text-center">
-		  	<h1>{{number_format((sizeof(\App\Presence::where('invitation_id','>',0)->where('event_id',Session::get('event_id'))->groupBy('invitation_id'))/(\App\Invitation::where('user_type_id',1)->where('event_id',Session::get('event_id'))->count()))*100,2)}}%</h1>
+		  	<h1>{{number_format((sizeof(\App\Presence::where('invitation_id','>',0)->where('event_id',Session::get('event_id'))->groupBy('invitation_id')->toArray())/(\App\Invitation::where('user_type_id',1)->where('event_id',Session::get('event_id'))->count()))*100,2)}}%</h1>
 		  </div>
 		  <div class="card-body">
 			  <div class="row">
@@ -36,7 +36,7 @@
 			  	</div>
 			  	<div class="col-md-6 text-center">
 			  		<div class="col-md-12">CHECK IN</div>
-			  		<div class="col-md-12"><h2><strong>{{sizeof(\App\Presence::where('invitation_id','>',0)->where('event_id',Session::get('event_id'))->groupBy('invitation_id')->count())}}</strong></h2></div>
+			  		<div class="col-md-12"><h2><strong>{{sizeof(\App\Presence::where('invitation_id','>',0)->where('event_id',Session::get('event_id'))->groupBy('invitation_id')->toArray())}}</strong></h2></div>
 			  	</div>
 			  </div>
 		  </div>
